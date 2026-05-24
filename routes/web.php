bbecbd053613d9c42401452310fe224b78b10c73
andriihnatenko-ux <andrii.hnatenko@oa.edu.ua>
2026-05-24 17:44:06 +0000

    lub3

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\RestTestController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified',
-])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
-});
+Route::apiResource('rest', RestTestController::class)->names('restTest');
