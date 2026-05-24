e7dfc41ebc613618230b854834404c994dd4ad25
andriihnatenko-ux <andrii.hnatenko@oa.edu.ua>
2026-05-24 17:37:32 +0000

    lub2

--- database/migrations/2026_05_24_164245_create_blog_categories_table.php
+++ database/migrations/2026_05_24_164245_create_blog_categories_table.php
@@ -6,20 +6,19 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('blog_categories', function (Blueprint $table) {
             $table->id();
+            $table->bigInteger('parent_id')->unsigned()->default(1);
+            $table->string('slug')->unique();
+            $table->string('title');
+            $table->text('description')->nullable();
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('blog_categories');
