9fd847742947a6dde82ac1f7e9ca1c1fd3489c45
andriihnatenko-ux <andrii.hnatenko@oa.edu.ua>
2026-06-05 15:52:09 +0000

    lub6

--- app/Http/Controllers/Api/Blog/Admin/CategoryController.php
+++ app/Http/Controllers/Api/Blog/Admin/CategoryController.php
@@ -2,48 +2,74 @@
 
 namespace App\Http\Controllers\Api\Blog\Admin;
 
-use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Models\BlogCategory;
+use Illuminate\Support\Str;
+use App\Http\Requests\BlogCategoryUpdateRequest;
+use App\Http\Requests\BlogCategoryCreateRequest;
+ use Illuminate\Http\Request;
 
-class CategoryController extends Controller
+class CategoryController extends BaseController
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function index()
     {
-        //
+        $paginator = BlogCategory::paginate(5);
+
+        return $paginator;
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
+    public function store(BlogCategoryCreateRequest $request)
     {
-        //
+        $data = $request->input(); // отримаємо масив даних, які надійшли з форми
+
+        if (empty($data['slug'])) { // якщо псевдонім порожній
+            $data['slug'] = Str::slug($data['title']); // генеруємо псевдонім
+        }
+
+        $item = (new BlogCategory())->create($data); // створюємо об'єкт і додаємо в БД
+
+        if ($item) {
+            return [
+                'success' => true,
+                'message' => 'Успішно збережено'
+            ];
+        } else {
+            return ['message' => 'Помилка збереження'];
+        }
     }
 
-    /**
-     * Display the specified resource.
-     */
     public function show(string $id)
     {
-        //
+         dd(__METHOD__);
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
+    public function update(BlogCategoryUpdateRequest $request, string $id)
     {
-        //
+        $item = BlogCategory::find($id);
+
+        if (empty($item)) {
+            return ['message' => "Запис id=[{$id}] не знайдено"];
+        }
+
+        $data = $request->all();
+
+        if (empty($data['slug'])) {
+            $data['slug'] = Str::slug($data['title']);
+        }
+
+        $result = $item->update($data);
+
+        if ($result) {
+            return [
+                'success' => true,
+                'message' => 'Успішно збережено'
+            ];
+        } else {
+            return ['message' => 'Помилка збереження'];
+        }
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
     public function destroy(string $id)
     {
-        //
+         dd(__METHOD__);
     }
 }
